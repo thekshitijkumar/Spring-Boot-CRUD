71e42da752fc25de4588996ee5c9a948c9e461ff
Kshitij <97kshitij@gmail.com>
2023-10-14 11:45:32 +0530

    Refactored Apis and removed sysout

--- src/main/java/com/kshitij/EmployeeTrackingApplication.java
+++ src/main/java/com/kshitij/EmployeeTrackingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmplyeeTrackingApplication {
+public class EmployeeTrackingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmplyeeTrackingApplication.class, args);
+		SpringApplication.run(EmployeeTrackingApplication.class, args);
 	}
 
 }
--- src/main/java/com/kshitij/service/EmployeeService.java
+++ src/main/java/com/kshitij/service/EmployeeService.java
@@ -3,39 +3,31 @@ package com.kshitij.service;
 import java.util.List;
 import java.util.Optional;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kshitij.model.Employee;
 import com.kshitij.repository.EmployeeRepository;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
 @Service
-@AllArgsConstructor
+@Slf4j
 public class EmployeeService {
-	
-	private final EmployeeRepository employeeRepository;
 
-	public void put(Employee req) {
-		Employee emp=new Employee();
-		emp.setEId(req.getEId());
-		emp.setAge(req.getAge());
-		emp.setName(req.getName());
-		emp.setAddress(req.getAddress());
-		
-		employeeRepository.save(emp);
-		
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	public Employee addEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+
 	}
 
-	public List<Employee> findEmp() {
-		List<Employee> emp=employeeRepository.findAll();
-		return emp;
+	public List<Employee> findAllEmployees() {
+		return employeeRepository.findAll();
 	}
 
 	public Optional<Employee> findEmpById(Integer id) {
-		Optional<Employee> emp=employeeRepository.findById(id);
-		return emp;
+		return employeeRepository.findById(id);
 		
 	}
 	
